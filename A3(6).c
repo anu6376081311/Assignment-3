176894eac119ddb63452f63679ab86296715edf8
anumeena <111556950+anu6376081311@users.noreply.github.com>
2023-03-13 01:25:50 +0530

    Add files via upload

---
+++ A3(6).c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int a,b;
+printf("Enter two number :");
+scanf("%d %d",&a,&b);
+if(a>=b)
+    printf("The grater number is %d",a);
+else
+printf("The  grater number is %d",b);
+return 0;
+}
