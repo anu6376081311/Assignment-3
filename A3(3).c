ca6baa227a4785af9ef0ab31a56d0ea6d9e8fba9
anumeena <111556950+anu6376081311@users.noreply.github.com>
2023-03-13 01:24:49 +0530

    Add files via upload

---
+++ A3(3).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number :");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("The given numer is a even number");
+    }
+    else
+    {
+        printf("The given no is a odd number");
+    }
+    return 0;
+}
